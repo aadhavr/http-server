0e76fe5610bb64ef8546d1e132205cb72f4eb9ad
aadhavr <aadhav.rajesh@gmail.com>
2024-07-19 23:10:02 -0700

    Respond with 200

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
@@ -8,8 +9,9 @@ fn main() {
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
                 println!("accepted new connection");
+                _stream.write_all("HTTP/1.1 200 OK\r\n\r\n");
             }
             Err(e) => {
                 println!("error: {}", e);
