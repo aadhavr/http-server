28821a3d185e73949b7964b2d95a3825a836aade
aadhavr <aadhav.rajesh@gmail.com>
2024-07-21 23:08:22 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,20 @@ fn main() {
                 let url_path = request_line.split_whitespace().nth(1).unwrap();
 
                 let response = if url_path == "/" {
-                    "HTTP/1.1 200 OK\r\n\r\n"
+                    "HTTP/1.1 200 OK\r\n\r\n".to_string()
+                } else if url_path.starts_with("/echo/") {
+                    let response_str = &url_path[6..];
+                    let content_length = response_str.len();
+                    format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                        content_length, response_str
+                    )
                 } else {
-                    "HTTP/1.1 404 Not Found\r\n\r\n"
+                    "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
                 };
 
                 let _ = stream.write_all(response.as_bytes()).unwrap();
+
             }
             Err(e) => {
                 println!("error: {}", e);
