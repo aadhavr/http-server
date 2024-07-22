e7286efbecbab6ceda78e4ac1141e5eb8fc90d69
aadhavr <aadhav.rajesh@gmail.com>
2024-07-21 23:24:45 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -21,27 +21,33 @@ fn main() {
                 let request = String::from_utf8_lossy(&buffer[..]);
                 let mut headers = request.lines();
                 
-                let request_line = request.lines().next().unwrap();
+                let request_line = headers.next().unwrap();
                 let url_path = request_line.split_whitespace().nth(1).unwrap();
 
-                if url_path == "/user-agent" {
+                if url_path == "/" {
+                    let response = "HTTP/1.1 200 OK\r\n\r\n".to_string();
+                    stream.write_all(response.as_bytes()).unwrap();
+                } else if url_path == "/user-agent" {
+                    // Initialize the User-Agent header value
                     let mut user_agent = "";
 
+                    // Loop through the headers to find the User-Agent header
                     for header in headers {
                         if header.to_lowercase().starts_with("user-agent:") {
-                            user_agent = header.split(":").nth(1).unwrap().trim();
+                            user_agent = header.split(':').nth(1).unwrap().trim();
                             break;
                         }
                     }
-                
 
+                    // Construct the response
                     let content_length = user_agent.len();
                     let response = format!(
-                            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                            content_length, user_agent
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                        content_length, user_agent
                     );
 
-                    let _ = stream.write_all(response.as_bytes()).unwrap();
+                    // Write the response to the stream
+                    stream.write_all(response.as_bytes()).unwrap();
                 } else if url_path.starts_with("/echo/") {
                     let response_str = &url_path[6..]; // Extract the string after "/echo/"
                     let content_length = response_str.len();
@@ -51,8 +57,9 @@ fn main() {
                     );
                     stream.write_all(response.as_bytes()).unwrap();
                 } else {
+                    // Respond with 404 Not Found for other paths
                     let response = "HTTP/1.1 404 Not Found\r\n\r\n".to_string();
-                    let _ = stream.write_all(response.as_bytes()).unwrap();
+                    stream.write_all(response.as_bytes()).unwrap();
                 }
             }
             Err(e) => {
